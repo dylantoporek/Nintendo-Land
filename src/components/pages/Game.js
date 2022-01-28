3c4061f1edfae01d9a47b0ffa4de2c9a7feee4cf
DylanToporek <dylantoporek@gmail.com>
2022-01-27 19:34:53 -0500

    piece movement updated

--- src/components/pages/Game.js
+++ src/components/pages/Game.js
@@ -45,60 +45,60 @@ function Game({game}){
 
     const navigate = useNavigate()
 
-useEffect(()=>{
-    if (game){
-        setPlayer({
-            name: "player",
-            avatar: game.player_avatar,
-            position: game.player_position
-        })
-    
-        setCpu1({
-            name: "cpu1",
-            avatar: game.cpu1_avatar,
-            position: game.cpu1_position
-        })
+    useEffect(()=>{
+        if (game){
+            setPlayer({
+                name: "player",
+                avatar: game.player_avatar,
+                position: game.player_position
+            })
         
-        setCpu2({
-            name: "cpu2",
-            avatar: game.cpu2_avatar,
-            position: game.cpu2_position
-        })
-    
-        setCpu3({
-            name: "cpu3",
-            avatar: game.cpu3_avatar,
-            position: game.cpu3_position
-        })
-
+            setCpu1({
+                name: "cpu1",
+                avatar: game.cpu1_avatar,
+                position: game.cpu1_position
+            })
+            
+            setCpu2({
+                name: "cpu2",
+                avatar: game.cpu2_avatar,
+                position: game.cpu2_position
+            })
         
-    } else {
-        alert('You must create a new game or load an existing game to play.')
-        navigate('/')
-    }
-        
-    
-}, [])
+            setCpu3({
+                name: "cpu3",
+                avatar: game.cpu3_avatar,
+                position: game.cpu3_position
+            })
 
-
-
-
-
-function handleSave(){
-    fetch(`/games/${game.id}`, {
-        method: "PATCH",
-        headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({player_position: player.position, cpu1_position: cpu1.position, cpu2_position: cpu2.position, cpu3_position: cpu3.position})
-    }).then(r => {
-        if(r.ok){
-            r.json().then( data=> alert('Game saved!'))
-        } else{
-            r.json().then(console.log('error saving game'))
+            
+        } else {
+            alert('You must create a new game or load an existing game to play.')
+            navigate('/')
         }
-    })
-}
+            
+        
+    }, [])
+
+
+
+
+
+    function handleSave(){
+        fetch(`/games/${game.id}`, {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({player_position: player.position, cpu1_position: cpu1.position, cpu2_position: cpu2.position, cpu3_position: cpu3.position})
+        }).then(r => {
+            if(r.ok){
+                r.json().then( data=> alert('Game saved!'))
+            } else{
+                r.json().then(console.log('error saving game'))
+            }
+        })
+    }
 
     function handleRoll(roll){
         const makePromise = (callback, player) => {
@@ -145,14 +145,32 @@ function handleSave(){
             steps = [player.position, player.position+1, player.position+2, player.position+3, player.position+4, player.position+5, player.position+6]
         } 
 
-        // console.log(steps)
+        // if(steps[steps.length - 1] === 3){
+        //     alert("Pipe")
+        // }
+        // if (steps[steps.length - 1] === 5){
+        //     alert("Bowser")
+        // }
+        // if (steps[steps.length - 1] === 11){
+        //     alert("Bokoblins")
+        // }
+        // if (steps[steps.length - 1] === 15){
+        //     alert("Nurse Joy")
+        // }
+        // if (steps[steps.length - 1] === 18){
+        //     console.log("bad effect Space 18")
+        // }
+        // if (steps[steps.length - 1] === 22){
+        //     console.log("bad effect Space 22")
+        // } 
+
         for( let i = 0; i < steps.length; i++){
-            // console.log(steps[i])
             setTimeout(() => setPlayer((player) => ({
                 ...player,
                 position: steps[i]
             })), (300 * (i+1)))
-        }       
+        } 
+             
     }
 
 
@@ -160,35 +178,154 @@ function handleSave(){
         let dice = [1, 2, 3, 4, 5, 6]
         let cpu1Roll = dice[Math.floor(Math.random()*6)]
         let cpu1Move = cpu1.position + cpu1Roll
-        setCpu1({
-            ...cpu1, 
-            position: cpu1Move
-        })
+        let steps = []
+        if (cpu1Roll === 1){
+            steps = [cpu1.position, cpu1.position+1]
+        }
+        if (cpu1Roll === 2){
+            steps = [cpu1.position, cpu1.position+1, cpu1.position+2]
+        }
+        if (cpu1Roll === 3){
+            steps = [cpu1.position, cpu1.position+1, cpu1.position+2, cpu1.position+3]
+        }
+        if (cpu1Roll === 4){
+            steps = [cpu1.position, cpu1.position+1, cpu1.position+2, cpu1.position+3, cpu1.position+4]
+        }
+        if (cpu1Roll === 5){
+            steps = [cpu1.position, cpu1.position+1, cpu1.position+2, cpu1.position+3, cpu1.position+4, cpu1.position+5]
+        } if (cpu1Roll === 6){
+            steps = [cpu1.position, cpu1.position+1, cpu1.position+2, cpu1.position+3, cpu1.position+4, cpu1.position+5, cpu1.position+6]
+        }
+
+        // if (steps[steps.length - 1] === 3){
+        //     alert("Pipe")
+        // }
+        // if (steps[steps.length - 1] === 5){
+        //     alert("Bowser")
+        // }
+        // if (steps[steps.length - 1] === 11){
+        //     alert("Bokoblins")
+        // }
+        // if (steps[steps.length - 1] === 15){
+        //     alert("Nurse Joy")
+        // }
+        // if (steps[steps.length - 1] === 18){
+        //     console.log("bad effect Space 18")
+        // }
+        // if (steps[steps.length - 1] === 22){
+        //     console.log("bad effect Space 22")
+        // }
+        
+        for( let i = 0; i < steps.length; i++){
+            setTimeout(() => setCpu1((cpu1) => ({
+                ...cpu1,
+                position: steps[i]
+            })), (300 * (i+1)))
+        } 
+        
     }
 
     function cpu2Roll(){
         let dice = [1, 2, 3, 4, 5, 6]
         let cpu2Roll = dice[Math.floor(Math.random()*6)]
         let cpu2Move = cpu2.position + cpu2Roll
-        setCpu2({
-            ...cpu2,
-            position: cpu2Move
-        })
+        let steps = []
+        if (cpu2Roll === 1){
+            steps = [cpu2.position, cpu2.position+1]
+        }
+        if (cpu2Roll === 2){
+            steps = [cpu2.position, cpu2.position+1, cpu2.position+2]
+        }
+        if (cpu2Roll === 3){
+            steps = [cpu2.position, cpu2.position+1, cpu2.position+2, cpu2.position+3]
+        }
+        if (cpu2Roll === 4){
+            steps = [cpu2.position, cpu2.position+1, cpu2.position+2, cpu2.position+3, cpu2.position+4]
+        }
+        if (cpu2Roll === 5){
+            steps = [cpu2.position, cpu2.position+1, cpu2.position+2, cpu2.position+3, cpu2.position+4, cpu2.position+5]
+        } if (cpu2Roll === 6){
+            steps = [cpu2.position, cpu2.position+1, cpu2.position+2, cpu2.position+3, cpu2.position+4, cpu2.position+5, cpu2.position+6]
+        } 
+
+        // if (steps[steps.length - 1] === 3){
+        //     alert("Pipe")
+        // }
+        // if (steps[steps.length - 1] === 5){
+        //     alert("Bowser")
+        // }
+        // if (steps[steps.length - 1] === 11){
+        //     alert("Bokoblins")
+        // }
+        // if (steps[steps.length - 1] === 15){
+        //     alert("Nurse Joy")
+        // }
+        // if (steps[steps.length - 1] === 18){
+        //     console.log("bad effect Space 18")
+        // }
+        // if (steps[steps.length - 1] === 22){
+        //     console.log("bad effect Space 22")
+        // }
+
+        for( let i = 0; i < steps.length; i++){
+            setTimeout(() => setCpu2((cpu2) => ({
+                ...cpu2,
+                position: steps[i]
+            })), (300 * (i+1)))
+        } 
+        
     }
 
     function cpu3Roll(){
         let dice = [1, 2, 3, 4, 5, 6]
         let cpu3Roll = dice[Math.floor(Math.random()*6)]
         let cpu3Move = cpu3.position + cpu3Roll
-        setCpu3({
-            ...cpu3,
-            position: cpu3Move
+        let steps = []
+        if (cpu3Roll === 1){
+            steps = [cpu3.position, cpu3.position+1]
+        }
+        if (cpu3Roll === 2){
+            steps = [cpu3.position, cpu3.position+1, cpu3.position+2]
+        }
+        if (cpu3Roll === 3){
+            steps = [cpu3.position, cpu3.position+1, cpu3.position+2, cpu3.position+3]
+        }
+        if (cpu3Roll === 4){
+            steps = [cpu3.position, cpu3.position+1, cpu3.position+2, cpu3.position+3, cpu3.position+4]
+        }
+        if (cpu3Roll === 5){
+            steps = [cpu3.position, cpu3.position+1, cpu3.position+2, cpu3.position+3, cpu3.position+4, cpu3.position+5]
+        } if (cpu3Roll === 6){
+            steps = [cpu3.position, cpu3.position+1, cpu3.position+2, cpu3.position+3, cpu3.position+4, cpu3.position+5, cpu3.position+6]
+        } 
 
-        })
+        // if(steps[steps.length - 1] === 3){
+        //     alert("Pipe")
+        // }
+        // if (steps[steps.length - 1] === 5){
+        //     alert("Bowser")
+        // }
+        // if (steps[steps.length - 1] === 11){
+        //     alert("Bokoblins")
+        // }
+        // if (steps[steps.length - 1] === 15){
+        //     alert("Nurse Joy")
+        // }
+        // if (steps[steps.length - 1] === 18){
+        //     console.log("bad effect Space 18")
+        // }
+        // if (steps[steps.length - 1] === 22){
+        //     console.log("bad effect Space 22")
+        // }
+
+        for( let i = 0; i < steps.length; i++){
+            setTimeout(() => setCpu3((cpu3) => ({
+                ...cpu3,
+                position: steps[i]
+            })), (300 * (i+1)))
+        } 
     }
 
-
-    console.log(player)
     const activePlayers = [
         player, cpu1, cpu2, cpu3
     ]
@@ -221,7 +358,6 @@ function handleSave(){
 
     return(
         <div>
-            {/* <img id='dice' src={dice} onClick={handleRoll}/> */}
             <div id='dice'>
             <Dice onRoll={handleRoll} size={80} />
             </div>
