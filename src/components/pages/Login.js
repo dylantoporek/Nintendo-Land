cfa3e88414293d1672141c0dc7639cf026b6bce9
DylanToporek <dylantoporek@gmail.com>
2022-02-12 14:29:13 -0500

    css updates for forms

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -10,9 +10,9 @@ function Login({onLogin}){
     const [showLogin, setShowLogin] = useState(true);
 
     return (
-      <div id='login-contianer'>
+      <div id='login-page-contianer'>
         {showLogin ? (
-          <>
+          <div id='login-div'>
             <LoginForm onLogin={onLogin} />
             <img src={donthaveaccount} id='signup-link'>
               
@@ -20,15 +20,15 @@ function Login({onLogin}){
             <button color="secondary" id='take-to-sign-up' onClick={() => setShowLogin(false)}>
                 <img id='sign-me-up' src={signuplabel}/>
             </button>
-          </>
+          </div>
         ) : (
-          <>
+          <div id='signin-din'>
             <SignUpForm onLogin={onLogin} />
             <img src={alreadyhaveaccount} id='login-link'/>
               <button id='take-to-log-in' color="secondary" onClick={() => setShowLogin(true)}>
                 <img id='log-me-in' src={loginlabel}/>
               </button>
-          </>
+          </div>
         )}
       </div>
     );
